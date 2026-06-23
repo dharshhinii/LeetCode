288a96a5e0da30c81fdf728cbfeabccd6e99a634
Dharshini <dharshhinii@gmail.com>
2026-06-23 23:06:08 +0530

    Delete 121-best-time-to-buy-and-sell-stock directory

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        m=prices[0]
-        p=0
-        for i in prices:
-            m=min(i,m)
-            p=max(p,i-m)
-        return p
